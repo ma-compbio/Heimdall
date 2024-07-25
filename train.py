0362613ca2d2bfc680c7095138f4712f88810f49
RemyLau <remylau961@gmail.com>
2024-07-25 15:30:16 -0400

    fix main tran script format

--- train.py
+++ train.py
@@ -1,29 +1,19 @@
-## loading in libraries
-import anndata as ad
 import hydra
-import numpy as np
-import pandas as pd
-import scanpy as sc
 import torch
 from datasets import Dataset
 from omegaconf import OmegaConf
 from sklearn.model_selection import train_test_split
-from torch.utils.data import DataLoader, TensorDataset
-from tqdm import tqdm
-from transformers import SchedulerType, get_scheduler
+from torch.utils.data import DataLoader
 
-## Cell representation tools from heimdall
 from Heimdall.cell_representations import CellRepresentation
 from Heimdall.f_c import geneformer_fc
 from Heimdall.f_g import identity_fg
-
-## initialize the model
 from Heimdall.models import HeimdallTransformer, TransformerConfig
 from Heimdall.trainer import HeimdallTrainer
 from Heimdall.utils import heimdall_collate_fn
 
 
-### using @hydra.main so that we can take in command line arguments
+# using @hydra.main so that we can take in command line arguments
 @hydra.main(config_path="config", config_name="config", version_base="1.3")
 def main(config):
     print(OmegaConf.to_yaml(config))
@@ -33,13 +23,13 @@ def main(config):
     # preprocess the dataset
     #####
 
-    CR = CellRepresentation(config)  ## takes in the whole config from hydra
-    CR.preprocess_anndata()  ## standard sc preprocessing can be done here
-    CR.preprocess_f_g(identity_fg)  ## takes in the identity f_g specified above
-    CR.preprocess_f_c(geneformer_fc)  ## takes in the geneformer f_c specified above
-    CR.prepare_labels()  ## prepares the labels
+    CR = CellRepresentation(config)  # takes in the whole config from hydra
+    CR.preprocess_anndata()  # standard sc preprocessing can be done here
+    CR.preprocess_f_g(identity_fg)  # takes in the identity f_g specified above
+    CR.preprocess_f_c(geneformer_fc)  # takes in the geneformer f_c specified above
+    CR.prepare_labels()  # prepares the labels
 
-    ## we can take this out here now and pass this into a PyTorch dataloader and separately create the model
+    # we can take this out here now and pass this into a PyTorch dataloader and separately create the model
     X = CR.cell_representation
     y = CR.labels
 
@@ -63,7 +53,8 @@ def main(config):
 
     # this is how you dynamically process your outputs into the right dataloader format
     # if you do not want conditional tokens, just omit those arguments
-    # what is crucial is that the dataset contains the arguments `inputs` and `labels`, anything else will be put into `conditional`
+    # what is crucial is that the dataset contains the arguments `inputs` and `labels`,
+    # anything else will be put into `conditional`
     ds_train = Dataset.from_dict(
         {"inputs": train_x, "labels": train_y, "conditional_tokens_1": train_x, "conditional_tokens_2": train_x},
     )
@@ -74,7 +65,7 @@ def main(config):
         {"inputs": test_x, "labels": test_y, "conditional_tokens_1": test_x, "conditional_tokens_2": test_x},
     )
 
-    ## this can probably be rolled into the train functionality itself, but lets keep it outside to be eaiser to debug
+    # this can probably be rolled into the train functionality itself, but lets keep it outside to be eaiser to debug
     dataloader_train = DataLoader(
         ds_train,
         batch_size=int(config.dataset.task_args.batchsize),
@@ -96,8 +87,8 @@ def main(config):
 
     ########
     # Create the model and the types of inputs that it may use
-    ## `type` can either be `learned`, which is integer tokens and learned nn.embeddings,
-    ##  or `predefined`, which expects the dataset to prepare batchsize x length x hidden_dim
+    # `type` can either be `learned`, which is integer tokens and learned nn.embeddings,
+    # or `predefined`, which expects the dataset to prepare batchsize x length x hidden_dim
     #######
 
     conditional_input_types = {
@@ -111,7 +102,7 @@ def main(config):
         },
     }
 
-    ## model config based on your specifications
+    # model config based on your specifications
     transformer_config = TransformerConfig(vocab_size=1000, max_seq_length=1000, prediction_dim=20)
     model = HeimdallTransformer(
         config=transformer_config,
@@ -119,14 +110,14 @@ def main(config):
         conditional_input_types=conditional_input_types,
     )
 
-    ## optimizer
+    # optimizer
     optimizer = torch.optim.AdamW(
         model.parameters(),
         lr=config.optimizer.learning_rate,
         weight_decay=config.optimizer.weight_decay,
         betas=(config.optimizer.beta1, config.optimizer.beta2),
         foreach=False,
-    )  ## the forearch is due to a distributed bug with cosine scheduler
+    )  # the forearch is due to a distributed bug with cosine scheduler
 
     #####
     # Initialize the Trainer
@@ -141,7 +132,7 @@ def main(config):
         run_wandb=True,
     )
 
-    ### Training
+    # Training
     trainer.train()
 
 
