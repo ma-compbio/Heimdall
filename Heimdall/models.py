87b77a3c39d5c2c2a6e8504f2be53c8bc221ac82
Shahul Alam <alam.shahul@gmail.com>
2024-12-17 23:21:39 -0500

    Refactor FlashAttention `TransformerEncoder`.

--- Heimdall/models.py
+++ Heimdall/models.py
@@ -8,6 +8,7 @@ import torch
 import torch.nn as nn
 from omegaconf import DictConfig
 from torch import Tensor
+from torch.nn import functional as F
 
 from Heimdall.cell_representations import CellRepresentation
 from Heimdall.datasets import PairedInstanceDataset
@@ -63,14 +64,14 @@ class HeimdallModel(nn.Module):
             conditional_input_types: Conditional input types specification.
 
         """
-        self.lm_model = instantiate_from_config(
+        self.encoder = instantiate_from_config(
             model_config,
             data,
             conditional_input_types,
         )
 
         self.num_labels = data.num_tasks
-        dim_in = self.lm_model.d_encoded
+        dim_in = self.encoder.d_encoded
 
         self.reducer = self.reduction_name = None
         if isinstance(data.datasets["full"], PairedInstanceDataset):
@@ -93,13 +94,13 @@ class HeimdallModel(nn.Module):
             first_cell_mask, second_cell_mask = attention_mask
 
             encoded_cells = tuple(
-                self.lm_model(cell_inputs, conditional_tokens, attention_mask=cell_mask)
+                self.encoder(cell_inputs, conditional_tokens, attention_mask=cell_mask)
                 for cell_inputs, cell_mask in zip(all_cell_inputs, attention_mask)
             )
 
             encoded = self.reducer(encoded_cells)
         else:
-            encoded = self.lm_model(inputs, conditional_tokens, attention_mask)
+            encoded = self.encoder(inputs, conditional_tokens, attention_mask)
 
         outputs = self.head(encoded)
 
@@ -133,71 +134,149 @@ class ExpressionOnly(nn.Module):
         return outputs
 
 
-class HeimdallTransformerEncoder(nn.Module):
+# class HeimdallTransformerEncoder(nn.Module):
+#
+#     def __init__(
+#         self,
+#         d_model: int,
+#         nhead: int,
+#         num_attention_heads: int,
+#         hidden_dropout_prob: float,
+#         use_flash_attn: bool,
+#         num_layers: int,
+#         hidden_act: str = "gelu",
+#     ):
+#         super().__init__()
+#
+#         self.use_flash_attn = use_flash_attn
+#
+#         if self.use_flash_attn:
+#             layer_constructor = FlashAttentionTransformerEncoderLayer
+#             encoder_constructor = FlashAttentionTransformerEncoder
+#             # try:
+#             #     from flash_attn.models.bert import BertEncoder as FABertEncoder
+#             #     from transformers import BertConfig
+#
+#             #     print("FlashAttention Library Successfully Loaded")
+#             # except ImportError:
+#             #     print(
+#             #         "Warning: FlashAttention Not Installed, "
+#             #         "when initializing model make sure to use default Transformers",
+#             #     )
+#
+#             # fa_config = BertConfig(
+#             #     hidden_size=d_model,
+#             #     num_hidden_layers=num_encoder_layers,
+#             #     num_attention_heads=nhead,
+#             #     intermediate_size=d_model * 4,
+#             #     hidden_act=hidden_act,
+#             #     hidden_dropout_prob=hidden_dropout_prob,
+#             #     attention_probs_dropout_prob=hidden_dropout_prob,
+#             #     use_flash_attn=True,  # use this to toggle between flash attention and not
+#             # )
+#             # self.encoder = FABertEncoder(fa_config)
+#
+#         else:
+#             layer_constructor = nn.TransformerEncoderLayer
+#             encoder_constructor = nn.TransformerEncoder
+#
+#         # Encoder layers
+#         encoder_layer = layer_constructor(
+#             d_model=d_model,
+#             nhead=nhead,
+#             dim_feedforward=d_model * 4,
+#             dropout=hidden_dropout_prob,
+#             activation=hidden_act,
+#             batch_first=True,
+#             norm_first=True,  # BERT uses LayerNorm before self-attention and feedforward networks
+#         )
+#         self.encoder = encoder_constructor(encoder_layer, num_layers=num_layers)
+#
+#     def forward(self, input_embeds, attention_mask):
+#         # Encoder
+#         if self.use_flash_attn:
+#             encoder_output = self.encoder(
+#                 input_embeds,
+#                 key_padding_mask=~attention_mask,  # FA uses a flipped mask
+#             )
+#         else:
+#             encoder_output = self.encoder(
+#                 input_embeds,
+#                 src_key_padding_mask=attention_mask,
+#             )
+#         return encoder_output
 
+
+class FlashAttentionTransformerEncoderLayer(nn.Module):
     def __init__(
         self,
-        d_model: int,
-        nhead: int,
-        num_attention_heads: int,
-        hidden_dropout_prob: float,
-        use_flash_attn: bool,
-        num_encoder_layers: int,
-        hidden_act: str = "gelu",
+        d_model,
+        nhead,
+        dim_feedforward: int = 2048,
+        dropout: float = 0.1,
+        norm_first: bool = False,
+        batch_first: bool = False,
     ):
+
         super().__init__()
 
-        self.use_flash_attn = use_flash_attn
+        self.self_attn = nn.MultiheadAttention(d_model, nhead, dropout=dropout, batch_first=batch_first)
+        self.linear1 = nn.Linear(d_model, dim_feedforward)
+        self.dropout = nn.Dropout(dropout)
+        self.linear2 = nn.Linear(dim_feedforward, d_model)
 
-        if self.use_flash_attn:
-            try:
-                from flash_attn.models.bert import BertEncoder as FABertEncoder
-                from transformers import BertConfig
+        self.norm1 = nn.LayerNorm(d_model)
+        self.norm2 = nn.LayerNorm(d_model)
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
 
-                print("FlashAttention Library Successfully Loaded")
-            except ImportError:
-                print(
-                    "Warning: FlashAttention Not Installed, "
-                    "when initializing model make sure to use default Transformers",
-                )
+        self.norm_first = norm_first
+        self.batch_first = batch_first
 
-            fa_config = BertConfig(
-                hidden_size=d_model,
-                num_hidden_layers=num_encoder_layers,
-                num_attention_heads=nhead,
-                intermediate_size=d_model * 4,
-                hidden_act=hidden_act,
-                hidden_dropout_prob=hidden_dropout_prob,
-                attention_probs_dropout_prob=hidden_dropout_prob,
-                use_flash_attn=True,  # use this to toggle between flash attention and not
-            )
-            self.encoder = FABertEncoder(fa_config)
+    def forward(self, src, src_mask=None, src_key_padding_mask=None):
+        if self.norm_first:
+            src = self.norm1(src)
+            with torch.backends.cuda.sdp_kernel(enable_flash=True):
+                src2 = self.self_attn(
+                    src,
+                    src,
+                    src,
+                    attn_mask=src_mask,
+                    key_padding_mask=src_key_padding_mask,
+                )[0]
+            src = src + self.dropout1(src2)
+
+            src = self.norm2(src)
+            src2 = self.linear2(self.dropout(F.relu(self.linear1(src))))
+            src = src + self.dropout2(src2)
         else:
-            # Encoder layers
-            encoder_layer = nn.TransformerEncoderLayer(
-                d_model=d_model,
-                nhead=nhead,
-                dim_feedforward=d_model * 4,
-                dropout=hidden_dropout_prob,
-                activation=hidden_act,
-                batch_first=True,
-                norm_first=True,  # BERT uses LayerNorm before self-attention and feedforward networks
-            )
-            self.encoder = nn.TransformerEncoder(encoder_layer, num_layers=num_encoder_layers)
+            with torch.backends.cuda.sdp_kernel(enable_flash=True):
+                src2 = self.self_attn(
+                    src,
+                    src,
+                    src,
+                    attn_mask=src_mask,
+                    key_padding_mask=src_key_padding_mask,
+                )[0]
+            src = src + self.dropout1(src2)
+            src = self.norm1(src)
+            src2 = self.linear2(self.dropout(F.relu(self.linear1(src))))
+            src = src + self.dropout2(src2)
+            src = self.norm2(src)
 
-    def forward(self, input_embeds, attention_mask):
-        # Encoder
-        if self.use_flash_attn:
-            encoder_output = self.encoder(
-                input_embeds,
-                key_padding_mask=~attention_mask,  # FA uses a flipped mask
-            )
-        else:
-            encoder_output = self.encoder(
-                input_embeds,
-                src_key_padding_mask=attention_mask,
-            )
-        return encoder_output
+        return src
+
+
+class FlashAttentionTransformerEncoder(nn.Module):
+    def __init__(self, encoder_layer, num_layers):
+        super().__init__()
+        self.layers = nn.ModuleList([encoder_layer for _ in range(num_layers)])
+
+    def forward(self, src, src_mask=None, src_key_padding_mask=None):
+        output = src
+        for layer in self.layers:
+            output = layer(output, src_mask=src_mask, src_key_padding_mask=src_key_padding_mask)
+        return output
 
 
 class HeimdallTransformer(nn.Module):
@@ -207,13 +286,9 @@ class HeimdallTransformer(nn.Module):
         conditional_input_types: Optional[dict],
         d_model: int,
         pos_enc: str,
-        nhead: int,
-        hidden_dropout_prob: float,
-        attention_probs_dropout_prob: float,
         pooling: str,
-        hidden_act: str,
-        use_flash_attn: bool,
-        num_encoder_layers: int,
+        encoder_layer_parameters: dict,
+        encoder_parameters: dict,
     ):
         super().__init__()
         """Heimdall transformer model.
@@ -244,7 +319,6 @@ class HeimdallTransformer(nn.Module):
         self.conditional_input_types = conditional_input_types
 
         self.fc = data.fc
-        self.use_flash_attn = use_flash_attn
 
         self.vocab_size = data.sequence_length + 2  # <PAD> and <MASK> TODO: data.vocab_size
 
@@ -285,15 +359,29 @@ class HeimdallTransformer(nn.Module):
                     raise ValueError(f"conditional_input_types.{name}['type'] must be either 'learned' or 'predefined'")
 
         # # Encoder
-        self.encoder = HeimdallTransformerEncoder(
-            d_model=d_model,
-            nhead=nhead,
-            num_attention_heads=num_encoder_layers,
-            hidden_dropout_prob=hidden_dropout_prob,
-            use_flash_attn=use_flash_attn,
-            hidden_act=hidden_act,
-            num_encoder_layers=num_encoder_layers,
-        )
+        # self.encoder = HeimdallTransformerEncoder(
+        #     d_model=d_model,
+        #     nhead=nhead,
+        #     num_attention_heads=num_encoder_layers,
+        #     hidden_dropout_prob=hidden_dropout_prob,
+        #     use_flash_attn=use_flash_attn,
+        #     hidden_act=hidden_act,
+        #     num_encoder_layers=num_encoder_layers,
+        # )
+
+        encoder_layer = instantiate_from_config(encoder_layer_parameters)
+        self.transformer_encoder = instantiate_from_config(encoder_parameters, encoder_layer)
+
+        # encoder_layer = layer_constructor(
+        #     d_model=d_model,
+        #     nhead=nhead,
+        #     dim_feedforward=d_model * 4,
+        #     dropout=hidden_dropout_prob,
+        #     activation=hidden_act,
+        #     batch_first=True,
+        #     norm_first=True,  # BERT uses LayerNorm before self-attention and feedforward networks
+        # )
+        # self.encoder = encoder_constructor(encoder_layer, num_layers=num_layers)
 
         # Initialize the [CLS] token as a learnable parameter
         self.cls_token = nn.Parameter(torch.zeros(1, 1, d_model))
@@ -368,8 +456,8 @@ class HeimdallTransformer(nn.Module):
             attention_mask = torch.cat([cls_attention, attention_mask], dim=1)  # Shape: (batch_size, seq_len + 1)
 
         # Encoder
-        encoder_output = self.encoder(input_embeds, attention_mask)
-        return encoder_output
+        transformer_encoder_output = self.transformer_encoder(input_embeds, src_key_padding_mask=attention_mask)
+        return transformer_encoder_output
 
 
 class CellPredHeadMixin:
--- tests/test_model.py
+++ tests/test_model.py
@@ -67,13 +67,21 @@ def paired_task_config(request, toy_paried_data_path):
       args:
         d_model: 128
         pos_enc: BERT
-        num_encoder_layers: 2
-        nhead: 2
-        hidden_act: gelu
-        hidden_dropout_prob: 0.1
-        attention_probs_dropout_prob: 0.1
-        use_flash_attn: False
-        pooling: cls_pooling # or "mean_pooling"
+        pooling: cls_pooling
+        encoder_layer_parameters:
+          type: torch.nn.TransformerEncoderLayer
+          args:
+            d_model: 128
+            nhead: 2
+            activation: gelu
+            dropout: 0.1
+            dim_feedforward: 512
+            batch_first: True
+            norm_first: True
+        encoder_parameters:
+          type: torch.nn.TransformerEncoder
+          args:
+            num_layers: 2
     dataset:
       dataset_name: zeng_merfish_ccc_subset
       preprocess_args:
@@ -246,6 +254,110 @@ def single_task_config(toy_single_data_path):
     return conf
 
 
+@fixture(scope="module")
+def flash_attention_config(toy_single_data_path):
+    config_string = f"""
+    project_name: Cell_Type_Classification_dev
+    run_name: run_name
+    work_dir: work_dir
+    seed: 42
+    float_dtype: 'float32'
+    data_path: null
+    ensembl_dir: null
+    cache_preprocessed_dataset_dir: null
+    entity: Heimdall
+    model:
+      type: Heimdall.models.HeimdallTransformer
+      name: transformer
+      args:
+        d_model: 128
+        pos_enc: BERT
+        pooling: cls_pooling
+        encoder_layer_parameters:
+          type: Heimdall.model.FlashAttentionTransformerEncoderLayer
+          args:
+            d_model: 128
+            nhead: 4
+            activation: gelu
+            dropout: 0.1
+            dim_feedforward: 512
+            batch_first: True
+            norm_first: True
+        encoder_parameters:
+          type: Heimdall.model.FlashAttentionTransformerEncoder
+          args:
+            num_layers: 6
+    dataset:
+      dataset_name: cell_type_classification
+      preprocess_args:
+        data_path: {toy_single_data_path}
+        top_n_genes: 1000
+        normalize: true
+        log_1p: true
+        scale_data: false
+        species: mouse
+    tasks:
+      args:
+        task_type: multiclass
+        label_col_name: class
+        metrics:
+        - Accuracy
+        - MatthewsCorrCoef
+        train_split: 0.8
+        shuffle: true
+        batchsize: 32
+        epochs: 10
+        prediction_dim: 14
+        dataset_config:
+          type: Heimdall.datasets.SingleInstanceDataset
+        head_config:
+          type: Heimdall.models.ExpressionOnlyCellPredHead
+          args: null
+    scheduler:
+      name: cosine
+      lr_schedule_type: cosine
+      warmup_ratio: 0.1
+      num_epochs: 20
+    trainer:
+      accelerator: cuda
+      precision: 32-true
+      random_seed: 42
+      per_device_batch_size: 64
+      accumulate_grad_batches: 1
+      grad_norm_clip: 1.0
+      fastdev: false
+    optimizer:
+      name: AdamW
+      args:
+        lr: 0.0001
+        weight_decay: 0.1
+        betas:
+        - 0.9
+        - 0.95
+        foreach: false
+    fc:
+      type: Heimdall.fc.DummyFc
+    fe:
+      type: Heimdall.fe.DummyFe
+      args:
+        embedding_parameters:
+          type: torch.nn.Module
+        d_embedding: null
+    fg:
+      name: IdentityFg
+      type: Heimdall.fg.IdentityFg
+      args:
+        embedding_parameters:
+          type: torch.nn.module
+        d_embedding: null
+    loss:
+      name: CrossEntropyLoss
+    """
+    conf = OmegaConf.create(config_string)
+
+    return conf
+
+
 def instantiate_and_run_model(config):
     cr = CellRepresentation(config)  # takes in the whole config from hydra
 
@@ -266,6 +378,10 @@ def instantiate_and_run_model(config):
 
 def test_single_task_model_instantiation(single_task_config):
     instantiate_and_run_model(single_task_config)
+
+
+def flash_attention_instantiation(flash_attention_config):
+    instantiate_and_run_model(flash_attention_config)
 
 
 @pytest.mark.parametrize(
