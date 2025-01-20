de0f886f1f6c659b0c9c9a844d81ae17945cf06f
Shahul Alam <alam.shahul@gmail.com>
2025-01-20 16:56:49 -0500

    Add adjacency matrix to `MAIN_KEYS` for collate function.

--- Heimdall/utils.py
+++ Heimdall/utils.py
@@ -22,7 +22,7 @@ from torch import Tensor
 from torch.utils.data import default_collate
 from tqdm.auto import tqdm
 
-MAIN_KEYS = {"identity_inputs", "expression_inputs", "labels", "masks", "expression_padding"}
+MAIN_KEYS = {"identity_inputs", "expression_inputs", "labels", "masks", "expression_padding", "adjacency_matrix"}
 
 
 def hash_config(cfg: DictConfig) -> str:
