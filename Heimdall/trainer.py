de76d350477d42f35e4f3721173780dd78a8101a
Shahul Alam <alam.shahul@gmail.com>
2025-09-23 13:01:48 -0400

    Style fix.

--- Heimdall/trainer.py
+++ Heimdall/trainer.py
@@ -8,7 +8,6 @@ import numpy as np
 import pandas as pd
 import psutil
 import torch
-import torch.nn as nn
 from accelerate import Accelerator
 from accelerate.utils import set_seed
 from omegaconf import OmegaConf
